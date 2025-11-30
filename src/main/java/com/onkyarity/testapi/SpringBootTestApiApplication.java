dbc89233f7e65f3f695d43c166bfd99700ca10e3
Techie Onkar Ambhorkar <techie.onkar.ambhorkar@gmail.com>
2025-11-30 11:36:46 +0530

    Initial commit: Set up Spring Boot project
    - Added Maven wrapper files (mvnw, mvnw.cmd, .mvn/)
    - Configured pom.xml with Spring Boot dependencies
    - Added .gitignore and .gitattributes
    - Set up basic project structure in src/ directory

---
+++ src/main/java/com/onkyarity/testapi/SpringBootTestApiApplication.java
@@ -0,0 +1,13 @@
+package com.onkyarity.testapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestApiApplication.class, args);
+	}
+
+}
