f070939be9a339016667c6b55d00e6e7d0fc8cc6
Techie Onkar Ambhorkar <techie.onkar.ambhorkar@gmail.com>
2025-12-07 19:13:55 +0530

    fix: update test configuration and logging
    - Added @ActiveProfiles("test") to TestApiApplicationTests
    - Created application-test.yml with test-specific logging config
    - Configured Maven test resources and surefire plugin
    - Added target/logs/ to .gitignore
    - Fixed test logging to use local directory instead of /app/logs

--- src/test/java/com/onkyarity/testapi/TestApiApplicationTests.java
+++ src/test/java/com/onkyarity/testapi/TestApiApplicationTests.java
@@ -2,12 +2,12 @@ package com.onkyarity.testapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TestApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {
+  }
 }
